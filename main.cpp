7aec7e55da2bcab6683313dd228084e2cf2e723b
mia16 <mahnur.imran@gmail.com>
2024-05-05 00:49:06 -0700

    added comments

--- main.cpp
+++ main.cpp
@@ -10,6 +10,17 @@ int main() {
     const int screen_height = 800;
     const string title = "Noor's Pong Game!";
     InitWindow(screen_width, screen_height, title.c_str());
+    //By defining frames per second, we make sure the game runs on the
+    //same speed on every computer
+    SetTargetFPS(60);
+
+    //WindowShouldClose() checks if the escape key on keyboard in pressed
+    //or if the close button on the window is pressed.
+    while(WindowShouldClose()==false){
+        BeginDrawing();
+
+        EndDrawing();
+    }
 
     CloseWindow();
     return 0;    
