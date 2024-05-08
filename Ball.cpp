d69235b34d8339d0490263beb6748ec2d782fdf3
mia16 <mahnur.imran@gmail.com>
2024-05-08 12:07:45 -0700

    added Ball instance/set attributes

--- Ball.cpp
+++ Ball.cpp
@@ -1,5 +1,5 @@
 #include "Ball.h"
 
 void Ball::Draw(){
-    DrawCircle(x, y, 20, PINK);
+    DrawCircle(x, y, 20, PINK); 
 }
--- main.cpp
+++ main.cpp
@@ -13,19 +13,28 @@ int main() {
     const int rectangle_width = 25;
     const int rectangle_height = 120;
     const int y_center = (screen_height/2) - (rectangle_height/2);
+
+    Ball ball;
+    ball.radius = 20;
+    ball.x = screen_width/2;
+    ball.y = screen_height/2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
     InitWindow(screen_width, screen_height, title.c_str());
     //By defining frames per second, we make sure the game runs on the
     //same speed on every computer
     SetTargetFPS(60);
 
-    //WindowShouldClose() checks if the escape key on keyboard in pressed
-    //or if the close button on the window is pressed.
+    //WindowShouldClose() checks if the escape key/close button pressed
     while(WindowShouldClose()==false){
         BeginDrawing();
         //Drawing
         DrawLine(screen_width/2, 0, screen_width/2, screen_width, GRAY);
+        ball.Draw();
         DrawRectangle(10, y_center, rectangle_width, rectangle_height, MAGENTA);
         DrawRectangle(1245, y_center, rectangle_width, rectangle_height, MAGENTA);
+        //End Drawing
         EndDrawing();
     }
 
